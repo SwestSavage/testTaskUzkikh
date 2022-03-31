e885570bf17f219525202baad9153b9c04f9ce41
Ilya Uzkikh <ilyauzkikh@gmail.com>
2022-03-31 19:18:06 +0300

    Configured adding new input field for checking if unp exists

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,77 +1,73 @@
-import React, { Component } from 'react';
+import React from "react";
+import './style.css'
 
-export default class App extends Component {
-    static displayName = App.name;
-
+export default class App extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = { unpData: {}, loading: true, unpNum: "", unpExist: false };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.renderForm = this.renderForm.bind(this);
+        super(props)
+        this.state = {
+            formValues: [{ unp: "", exist: "" }]
+        };
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    handleChange(event) {
-        const exist = this.checkIfUnpExist(event.target.value);
-
-        this.setState({ unpNum: event.target.value, unpExist: exist.unpExist });
+    handleChange(i, e) {
+        let formValues = this.state.formValues;
+        formValues[i][e.target.name] = e.target.value;
+        formValues[i]["exist"] = this.checkIfUnpExist(i, e.target.value);
+        this.setState({ formValues });
     }
 
-    componentDidMount() {
-        this.fetchUnp();
-    }   
-
-    static renderUnp(unp) {
-        return (
-            <p>{ unp.vnaimp }</p>
-            )
+    addFormFields() {
+        this.setState(({
+            formValues: [...this.state.formValues, { unp: "", exist: "" }]
+        }))
     }
 
-    renderForm() {
-        return (
-            <form>
-                <label>
-                    UNP:
-                    <input type="number" value={this.state.unpNum} onChange={this.handleChange} />
-                </label>
-                <p>Unp exist in db: {this.state.unpExist === true ? "Yes" : "No"}</p>
-            </form>
-            )
+    removeFormFields(i) {
+        let formValues = this.state.formValues;
+        formValues.splice(i, 1);
+        this.setState({ formValues });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        alert(JSON.stringify(this.state.formValues));
     }
 
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : App.renderUnp(this.state.unpData);
-
-        let form = this.renderForm();
 
         return (
-            <div>
-                <h1 id="tabelLabel" >Check UNP</h1>
-                {contents}
-                {form}
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                {this.state.formValues.map((element, index) => (
+                    <div className="form-inline" key={index}>
+                        <label>Unp</label>
+                        <input type="number" name="unp" value={element.unp || ""} onChange={e => this.handleChange(index, e)} />
+                        <label>Unp exist in db: {this.state.formValues[index]["exist"] === true ? "Yes" : "No"}</label>
+                        {
+                            index ?
+                                <button type="button" className="button remove" onClick={() => this.removeFormFields(index)}>Remove</button>
+                                : null
+                        }
+                    </div>
+                ))}
+                <div className="button-section">
+                    <button className="button add" type="button" onClick={() => this.addFormFields()}>Add</button>
+                    <button className="button submit" type="submit">Submit</button>
+                </div>
+            </form>
         );
     }
 
-    async fetchUnp() {
-        const response = await fetch('api/unp/getData?unp=123456789&type=json');
-        console.log(response);
-
-        const data = await response.json();
-
-        console.log("Obj: " + JSON.stringify(data));
-
-        this.setState({ unpData: data, loading: false });
-    }
-
-    async checkIfUnpExist(unpNum) {
+    async checkIfUnpExist(i, unpNum) {
         const response = await fetch('api/unp/checkUnp?unp=' + unpNum);
         const data = await response.json();
 
         console.log(JSON.stringify(data));
-        this.setState({ unpExist: data.unpExist });
+
+        let formValues = this.state.formValues;
+        formValues[i]["exist"] = data.unpExist;
+
+        this.setState({ formValues });
 
         return data;
     }
